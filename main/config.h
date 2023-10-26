8cf4efbb5e2fe7b89966fd29efa05cc81fc897fe
reepier <pierre.vezin98@gmail.com>
2023-10-27 00:11:47 +0200

    Code is now modular

---
+++ main/config.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <DMXSerial.h>
+
+#define red_pin 3
+#define green_pin 5
+#define blue_pin 6
+#define alim_laser 4
+#define motor_1 10
+#define motor_2 9
+
+const unsigned long d_anim = 20000; //ms
+
+const unsigned long d_pause = 20000; //ms
+const unsigned long d_pause_mini = 2000; //ms 
